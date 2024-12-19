77e0bbb84c0d64c9e8d0b9fb4d3df0a19ba5a921
Assav <Assav@Assav>
2024-12-19 10:59:48 +0700

    install commit

---
+++ src/main/java/com/mycompany/loginin/Loginin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loginin;
+
+/**
+ *
+ * @author Assav
+ */
+public class Loginin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
